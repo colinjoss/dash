7398126cfde7718cbaff1d69c8e712b25c1291e3
Colin Joss <colindjoss@gmail.com>
2021-02-02 13:49:22 -0800

    Added upload_file and move_file funcs

--- main.py
+++ main.py
@@ -5,11 +5,12 @@
 import datetime
 import json
 import os
+import shutil
+import inquirer
 import calendar
 
-
-my_date = datetime.date(2021, 2, 2)
-print(calendar.day_name[my_date.weekday()])
+# print(calendar.setfirstweekday(6))
+# print(calendar.month(2021, 2))
 
 
 class Diary:
@@ -35,7 +36,7 @@ class Diary:
         return self._entries[-1]
 
     def update_diary(self):
-        """Records a new diary entry."""
+        """Records new diary entry(s)."""
         last_entry = self.get_last_entry()
         if last_entry is None:
             return
@@ -47,6 +48,8 @@ class Diary:
         self.new_entry(self.get_date(), self.get_weekday())
 
     def catch_up(self, last_date):
+        """If update_diary determines the current date and the last date the diary was updated
+        don't match, catch_up is called to prompt the user to update for multiple previous days."""
         match = False
         catch_up_days = []
         while match is False:
@@ -58,11 +61,13 @@ class Diary:
 
         for day in catch_up_days:
             date = day
-            year, month, day = self.split_date()
+            year, month, day = self.split_date(date)
             weekday = datetime.date(year, month, day).strftime("%A")
             self.new_entry(day, weekday)
 
     def new_entry(self, day, weekday):
+        """Prompts the user through the components of a diary entry, and
+        then adds the entry to the list of all entries."""
         date = day
         weekday = weekday
         file = self.upload_file()
@@ -75,14 +80,47 @@ class Diary:
                               "happiness": happiness})
 
     def split_date(self, date):
+        """Splits a hyphenated date into its year/month/day parts, and returns each as an int."""
         date_list = str(date).split("-")
         return int(date_list[0]), int(date_list[1]), int(date_list[2])
 
-    def upload_file(self):
-        pass
-        # os.chdir(f"/Users/Desktop/Master Folder/Projects/Diary/{date.today().year}/")
+    def upload_file(self, date, weekday):
+        confirm = self.list_selection(["Yes", "No"],
+                                      f"Would you like to upload a file for {weekday}, {date}?")
+        if confirm == "No":
+            return
+
+        os.chdir(os.getcwd() + "\\new-update-files")
+        files = []
+        files += os.listdir()
+        if not files:
+            return print("There are no files to upload.")
+        files.append("Cancel")
+
+        selection = self.list_selection(files, "Which file?")
+        if selection == "Cancel":
+            return
+
+        dest = f'C:\\Users\\Colin\\Desktop\\Master Folder\\Projects\\Diary\\{datetime.date.today().year}'
+        self.move_file(selection, dest)
+        return f"{dest}\\selection"
+
+    def move_file(self, file, dest):
+        src = f'{os.getcwd()}\\new-update-files\\'
+        shutil.copy(src + file, dest)
+
+    def list_selection(self, choices, message=""):
+        options = [
+            inquirer.List('list',
+                          message=message,
+                          choices=choices
+                          )]
+        selection = inquirer.prompt(options)
+        return selection["list"]
+
 
     def get_summary_from_user(self, date, weekday):
+        """Prompts the user through a detailed summary for a given date."""
         print(f"This is the summary for {weekday}, {date}.")
         print("Remember to be as detailed as possible - and to use as many "
               "KEYWORDS as you can!")
@@ -98,6 +136,4 @@ class Diary:
 
 if __name__ == '__main__':
     test = Diary()
-    date = str(test.get_date())
-    year, month, day = test.split_date(date)
-    print(datetime.date(year, month, day).strftime("%B"))
+    # print(test.get_summary_from_user(datetime.date.today(), "Tuesday"))
