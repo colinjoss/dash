69a45723930eace5cb5c5a9e919865e8cdf84771
Colin Joss <colindjoss@gmail.com>
2021-02-01 16:02:14 -0800

    Added init func to Diary class

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # Author: Colin Joss
 # Last date updated: 2-1-2021
-# Description: A simple command prompt interface for my personal diary system.
+# Description: A simple python program for my personal diary system, made with the intention to
+#              speed up the process of maintaining it.
+from datetime import date
+import json
+
+
+class Diary:
+    def __init__(self):
+        with open("save_data.json", "r") as infile:
+            data = json.load(infile)
+            self._entries = data[0]
+        self._date = date.today().strftime("%m-%d-%Y")
+        self._day_of_the_week = date.today().strftime("%A")
+
 
 if __name__ == '__main__':
     pass
